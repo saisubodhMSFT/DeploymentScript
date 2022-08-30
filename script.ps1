6f858304f09571edd123d3ce6ad42def9c68270e
saisubodhMSFT <90855770+saisubodhMSFT@users.noreply.github.com>
2022-08-31 00:35:32 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,7 +1,1 @@
-param(
-[string] [Parameter(Mandatory=$true)] $resourceGroup,
-[string] [Parameter(Mandatory=$true)] $afdProfile,
-[string] [Parameter(Mandatory=$true)] $afdEndpoint
-)
-
-$afdHostNameOutputLine = az afd endpoint show -g testafd103 --endpoint-name testafd103-afdendpoint --profile-name testafd103-afdprofile-7acaba4f090fa4385b46 | Select-String -Pattern '( )*"hostName":( )*"[a-z]([a-z0-9]|-)*.z01.azurefd.net"( )*,(.)*'
+Write-Output "success"
